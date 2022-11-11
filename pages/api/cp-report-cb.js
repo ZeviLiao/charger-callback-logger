071a3584b1075330c7e4b0466fe9ef792bd4a3c1
Zevi Liao <zevi.liao@dgpals.io>
2022-11-11 21:01:08 +0800

    update response.

--- pages/api/cp-report-cb.js
+++ pages/api/cp-report-cb.js
@@ -6,5 +6,5 @@ export default function handler(req, res) {
   logger.info(`Request headers: ${JSON.stringify(req.headers)}`);
   logger.info(`Request body: ${JSON.stringify(req.body)}`);
 
-  res.status(200).json({ name: 'John Doe' });
+  res.status(200).json({ response: req.body });
 }
